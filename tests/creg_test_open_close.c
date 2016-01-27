e0a4f5fe4afbbc3ad3a12e2402d3a699b59ce4ab
Joachim Metz <joachim.metz@gmail.com>
2016-01-27 07:52:52 +0100

    Worked on tests.

--- tests/creg_test_open_close.c
+++ tests/creg_test_open_close.c
@@ -20,12 +20,11 @@
  */
 
 #include <common.h>
+#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
 #endif
-
-#include <stdio.h>
 
 #include "creg_test_libcerror.h"
 #include "creg_test_libcreg.h"
