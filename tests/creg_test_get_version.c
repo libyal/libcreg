79532e597f9d8b89b20a86f640dce2a88fe5b4f6
Joachim Metz <joachim.metz@gmail.com>
2016-09-01 07:20:25 +0200

    Worked on tests.

--- tests/creg_test_get_version.c
+++ tests/creg_test_get_version.c
@@ -20,7 +20,6 @@
  */
 
 #include <common.h>
-#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
@@ -28,6 +27,7 @@
 
 #include "creg_test_libcstring.h"
 #include "creg_test_libcreg.h"
+#include "creg_test_macros.h"
 #include "creg_test_unused.h"
 
 /* Tests retrieving the library version
@@ -46,11 +46,15 @@ int creg_test_get_version(
 	          LIBCREG_VERSION_STRING,
 	          9 );
 
-	if( result != 0 )
-	{
-		return( 0 );
-	}
+	CREG_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
 	return( 1 );
+
+on_error:
+	return( 0 );
 }
 
 /* The main program
@@ -68,10 +72,13 @@ int main(
 	CREG_TEST_UNREFERENCED_PARAMETER( argc )
 	CREG_TEST_UNREFERENCED_PARAMETER( argv )
 
-	if( creg_test_get_version() != 1 )
-	{
-		return( EXIT_FAILURE );
-	}
+	CREG_TEST_RUN(
+	 "libcreg_get_version",
+	 creg_test_get_version() )
+
 	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
 }
 
--- tests/creg_test_memory.h
+++ tests/creg_test_memory.h
@@ -1,5 +1,5 @@
 /*
- * Notification functions
+ * Memory allocation functions for testing
  *
  * Copyright (C) 2013-2016, Joachim Metz <joachim.metz@gmail.com>
  *
@@ -19,45 +19,32 @@
  * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if !defined( _LIBCREG_NOTIFY_H )
-#define _LIBCREG_NOTIFY_H
+#if !defined( _CREG_TEST_MEMORY_H )
+#define _CREG_TEST_MEMORY_H
 
 #include <common.h>
-#include <file_stream.h>
-#include <types.h>
-
-#include "libcreg_extern.h"
-#include "libcreg_libcerror.h"
 
 #if defined( __cplusplus )
 extern "C" {
 #endif
 
-#if !defined( HAVE_LOCAL_LIBCREG )
+#if defined( HAVE_GNU_DL_DLSYM ) && defined( __GNUC__ ) && !defined( __clang__ )
 
-LIBCREG_EXTERN \
-void libcreg_notify_set_verbose(
-      int verbose );
+#define HAVE_CREG_TEST_MEMORY		1
 
-LIBCREG_EXTERN \
-int libcreg_notify_set_stream(
-     FILE *stream,
-     libcerror_error_t **error );
+extern int creg_test_malloc_attempts_before_fail;
 
-LIBCREG_EXTERN \
-int libcreg_notify_stream_open(
-     const char *filename,
-     libcerror_error_t **error );
+extern int creg_test_memcpy_attempts_before_fail;
 
-LIBCREG_EXTERN \
-int libcreg_notify_stream_close(
-     libcerror_error_t **error );
+extern int creg_test_memset_attempts_before_fail;
 
-#endif /* !defined( HAVE_LOCAL_LIBCREG ) */
+extern int creg_test_realloc_attempts_before_fail;
+
+#endif /* defined( HAVE_GNU_DL_DLSYM ) && defined( __GNUC__ ) && !defined( __clang__ ) */
 
 #if defined( __cplusplus )
 }
 #endif
 
-#endif /* !defined( _LIBCREG_NOTIFY_H ) */
+#endif /* !defined( _CREG_TEST_MEMORY_H ) */
 
