7a06230ee0dee7b85cbecba934b2ce6a0054caa9
Joachim Metz <joachim.metz@gmail.com>
2026-05-06 08:34:48 +0200

    Applied updates

--- cregtools/cregmount.c
+++ cregtools/cregmount.c
@@ -1,7 +1,7 @@
 /*
  * Mounts a Windows 9x/Me Registry File (CREG).
  *
- * Copyright (C) 2013-2025, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2013-2026, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
