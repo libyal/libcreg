114f8518bd65dcfb996d9a23fd28959759310d2c
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 18:14:32 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libcreg/.libs/libcreg.1.dylib && test -f ./pycreg/.libs/pycreg.so
+if [ -f "${PWD}/libcreg/.libs/libcreg.1.dylib" ] && [ -f ./pycreg/.libs/pycreg.so ]
 then
-	install_name_tool -change /usr/local/lib/libcreg.1.dylib ${PWD}/libcreg/.libs/libcreg.1.dylib ./pycreg/.libs/pycreg.so
+    install_name_tool -change /usr/local/lib/libcreg.1.dylib "${PWD}/libcreg/.libs/libcreg.1.dylib" ./pycreg/.libs/pycreg.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
